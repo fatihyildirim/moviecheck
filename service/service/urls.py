b59de2828ec72e27c9480eedecb38066104cbfda
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-05-16 14:43:03 +0300

    movie route added urls.py (service)

--- service/service/urls.py
+++ service/service/urls.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
+from movie import views as mv
 
 
 router = routers.DefaultRouter()
-
+router.register(r'movies', mv.MovieViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
