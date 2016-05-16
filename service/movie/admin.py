0930bebbf1ad54c151e969b248d3b75827f8ca7b
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-05-16 14:35:10 +0300

    model fields create and added admin (service)

--- service/movie/admin.py
+++ service/movie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from movie.models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+	list_display = ('name', 'year', 'runtime', 'director', 'status')
+	list_editable = ('status',)
+
+admin.site.register(Movie, MovieAdmin)
--- service/movie/models.py
+++ service/movie/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
+from django.db import models as m
 
-from django.db import models
 
-# Create your models here.
+class Movie(m.Model):
+	name = m.CharField(max_length=200)
+	year = m.IntegerField(blank=True, null=True)
+	runtime = m.IntegerField(blank=True, null=True)
+	director = m.CharField(max_length=200, blank=True, null=True)
+	status = m.BooleanField(default=False)
+
+	def __unicode__(self):
+		return '%s' % self.name
