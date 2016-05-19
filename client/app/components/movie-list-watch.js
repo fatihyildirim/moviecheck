1c6c14d95d3ce9ce9bfbae91722edc31043df60a
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-05-19 19:14:45 +0300

    multiple order added for watched movies (client)

--- client/app/components/movie-list-watch.js
+++ client/app/components/movie-list-watch.js
@@ -14,7 +14,26 @@ export default Ember.Component.extend({
       });
     });
   },
+  isFilter: false,
+  ordering: [],
   actions: {
+    order(type) {
+      let self = this;
+      if (self.get('ordering').indexOf(type) >= 0) {
+        self.get('ordering').splice(self.get('ordering').indexOf(type), 1);
+      } else {
+        self.get('ordering').push(type);
+      }
+
+      return self.get('store').query('movie', {ordering: self.get('ordering').join(",")}).then(function(movies) {
+        self.set('movies', []);
+        movies.map(item => {
+          if (item.get('status')) {
+            self.get('movies').pushObject(item);
+          }
+        });
+      });
+    },
     editMovie(name, year, runtime, director, id) {
       this.set('name', name);
       this.set('year', year);
