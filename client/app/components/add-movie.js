59cb5e0cfbf231cf3d6d87a4ee8e3e691529f75f
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-05-23 17:45:27 +0300

    initializer create for get route in component, movie create action move to component (client)

--- client/app/components/add-movie.js
+++ client/app/components/add-movie.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	store: Ember.inject.service(),
 	actions:{
 		save(){
-			this.sendAction('createMovie',
-				this.getProperties('name','year','runtime','director','writer')
-			);
+			let self = this;
+			self.get('store').createRecord('movie', {
+				name: self.get('name'),
+				year: self.get('year'),
+				runtime: self.get('runtime'),
+				director: self.get('director'),
+				writer: self.get('writer')
+			}).save().then(function(){
+				self.get('router').transitionTo('movies');
+			}.bind(self));
 		}
 	}
 });
--- client/app/routes/movies/create.js
+++ client/app/routes/movies/create.js
@@ -1,18 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	actions:{
-		createMovie(data){
-			console.log(data);
-			this.store.createRecord('movie', {
-				name: data.name,
-				year: data.year,
-				runtime: data.runtime,
-				director: data.director,
-				writer: data.director
-			}).save().then(function(){
-				this.transitionTo('movies');
-			}.bind(this));
-		}
-	}
+
 });
