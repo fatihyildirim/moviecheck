61ad3eb7441c1d677d3a1f2bbdc72f020a5594f8
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-05-24 17:26:02 +0300

    movie create form field changed and add director, writer relation (client)

--- client/app/components/add-movie.js
+++ client/app/components/add-movie.js
@@ -2,15 +2,48 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	store: Ember.inject.service(),
+	movies: [],
+	didInsertElement() {
+		let self = this;
+		this.get('store').findAll('movie').then(function(movies) {
+			self.set('movies', []);
+			movies.map(item => {
+				if (!item.get('status')) {
+					self.get('movies').pushObject(item);
+				}
+			});
+		});
+		Ember.$(".director-select").select2({
+		  maximumSelectionLength: 1,
+		});
+		Ember.$(".writer-select").select2({
+			maximumSelectionLength: 1,
+		});
+
+		Ember.$('.director-select').change(function() {
+			self.set('director', $(this).val());
+		});
+
+		Ember.$('.writer-select').change(function() {
+			self.set('writer', $(this).val());
+		});
+
+		// let data = $('.director-select').select2('data');
+	},
 	actions:{
 		save(){
 			let self = this;
+			let director = self.get('store').peekRecord('director', self.get('director'));
+			let writer = self.get('store').peekRecord('writer', self.get('writer'));
+			console.log(director);
+			console.log(writer);
+
 			self.get('store').createRecord('movie', {
 				name: self.get('name'),
 				year: self.get('year'),
 				runtime: self.get('runtime'),
-				director: self.get('director'),
-				writer: self.get('writer')
+				director: director,
+				writer: writer
 			}).save().then(function(){
 				self.get('router').transitionTo('movies');
 			}.bind(self));
--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -19,9 +19,13 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  
+
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+
+  app.import('bower_components/select2/select2.css');
+  app.import('bower_components/select2/select2.min.js');
+  
   app.import('vendor/font-awesome-4.6.2/css/font-awesome.css');
 
   return app.toTree();
